922a8678a6d71d2c53a09c3165beb038936cb3d1
blair c <blairhb7@yahoo.com>
2023-09-19 22:26:56 -0700

    tablet changes

--- app/Components/nav.tsx
+++ app/Components/nav.tsx
@@ -8,17 +8,17 @@ const nav = () => {
 
   return (
     <>
-    <main className=" w-screen bg-transparent fixed z-50">
+    <main className="  bg-transparent fixed z-50">
         <nav className=' flex justify-between py-4 px-2 uppercase'>
            <Link href={'/'}> 
                  <div className="flex flex-col">
-                    <h1 id='logo' className=" text-[.5em] md:text-base px-1 font-medium leading-tight text-white shadow-black outline">Blair Chappell <br/> <span className=' text-xslg:text-base'>frontend developer</span></h1>
+                    <h1 id='logo' className=" text-[.5em] md:text-sm px-1 font-medium leading-tight text-white shadow-black outline">Blair Chappell <br/> <span className=' text-xslg:text-base'>frontend developer</span></h1>
                  </div>
             </Link>
             <div className="">
             <header>
                 <input type='checkbox' id='toggle' className='hidden' />
-                <label className='toggle-btn ' htmlFor='toggle'>
+                <label className='toggle-btn px-1 ' htmlFor='toggle'>
                     <div className="bar"></div>
                     <div className="bar"></div>
                     <div className="bar"></div>
